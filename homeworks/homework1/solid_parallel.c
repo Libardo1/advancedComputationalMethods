06834dd7627283f760a045dd7c5f495da1324651
Ivan Torroledo <ivan.torroledo@gmail.com>
2017-02-25 18:33:50 -0500

    wrong results

---
+++ homeworks/homework1/solid_parallel.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+#include <stdlib.h>
+
+#define PI acos(-1.0)
+
+// Parameters
+
+double betta = 0.3;
+int N = 100;
+double delta_t  = 10E-3;
+
+// Define internal functions 
+
+double* init(double *array);
+void F(double * F_array, double * x);
+
+int main(){
+	
+  	// MPI initialization 
+
+  	MPI_Init(NULL,NULL);
+  	int size, rank;
+  	MPI_Comm_size(MPI_COMM_WORLD, &size);
+  	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  	// We have to ckeck if the number of processors is allowed : # must be 2^n n = 1,2,3,4
+ 
+	if(rank==0){
+		if (size != 2 & size != 4 & size != 8 & size != 16 & size != 1){
+			printf("\n\n\n%d is not a number of processors allowed", size);
+     		fprintf(stderr, "\nPlease run again with some of these numbers : 2, 4, 6, 8 \n\n");
+     		abort();
+     		MPI_Abort(MPI_COMM_WORLD, 1);
+    	}
+    	printf("\n\n\nWe have %d processors avaliables\n", size);
+  	}
+  
+  	int T = 100*N;
+	int i,j,k,t,n,cont = 0;
+  
+  	// Create the grids to store N points in the solid
+  
+  	double* x = malloc(N*sizeof(double));
+  	double* x_new = malloc(N*sizeof(double));
+  	double* v = malloc(N*sizeof(double));
+  	double* v_new = malloc(N*sizeof(double));
+  	double* F_grid = malloc(N*sizeof(double));
+  	double* F_grid_new = malloc(N*sizeof(double));
+  	
+  	// Create the total grid to store data
+  	
+	double **DATA = (double **) malloc(N * sizeof(double *));
+	for(i=0;i<N;i++){
+		DATA[i] = (double *) malloc(N * sizeof(double *));
+	}
+  	
+  	// Initial conditions for the solid 
+  
+	x = init(x);
+  	x_new = init(x_new);
+	
+	for(n=0;n<N;n++){
+		v[n] = 0.0;
+		v_new[n] = 0.0;
+		F_grid[n] = 0.0;
+		F_grid_new[n] =0.0; 
+	}
+	
+	// ***************************************************************************	//
+	// 							Leapfrog method										//
+	// ***************************************************************************	//
+	// We use the Leapfrog method expressed in x, v & a quantities with integer steps 
+	
+	for(t = 0; t < T; t++){
+		
+		// Update
+		
+		*x = *x_new;
+		*v = *v_new;
+		
+		F(F_grid,x);
+		for(n = 1; n < N-1; n++){
+			x_new[n] = x[n] + v[n] * delta_t + 0.5 * F_grid[n] * pow(delta_t,2);
+		}	
+		F(F_grid_new,x);
+		for(n = 1; n < N-1; n++){
+			v_new[n] = v[n] + 0.5 * (F_grid[n] + F_grid_new[n]) * delta_t;
+		}
+	
+		if(t%N == 0){
+			for(i = 0; i<N;i++){
+				DATA[i][cont] = x_new[i];
+			}
+			cont++;
+		}				
+	}
+	
+  	MPI_Finalize();	
+  	return(0);
+}
+
+double *init(double *array){ 
+  	int i;
+  	array[0] = 0.0;
+  	array[N-1] = 0.0;
+	for(i=1; i<N-1; i++){
+		array[i] = sin(2*PI*i/(double)(N-1));
+	}
+	return array;
+}
+
+void F(double * F_array, double * x){
+	int n;
+	for(n = 1; n < N-1; n++){
+		F_array[n] = (x[n+1] - 2.0 * x[n] + x[n-1]) + 
+				betta * ( pow(x[n+1] - x[n],3.0) - pow(x[n] - x[n-1],3) );
+	}
+}

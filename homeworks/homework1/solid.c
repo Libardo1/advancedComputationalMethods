502f754ac0419bab32b4c295033e028107d4a4ff
Iván Torroledo Peña <Torroledo@MacBook-Pro-de-Ivan-Torroledo-Pena.local>
2017-02-25 13:21:24 -0500

    init

---
+++ homeworks/homework1/solid.c
@@ -0,0 +1,17 @@
+#include 'stdio.h'
+#include 'math.h'
+#include 'mpi.h'
+#include 'stdlib.h'
+
+// Parameters
+
+double betta = 0.3;
+int N = 1024, T = N*100;
+
+
+void main(){
+  
+
+  MPI_INIT(NULL,NULL);
+
+}

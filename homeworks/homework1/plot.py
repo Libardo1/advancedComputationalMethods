e2c1a0c61f3ffebd9225716f5e3cd594ebfb356d
Ivan Torroledo <ivan.torroledo@gmail.com>
2017-03-08 09:15:42 -0500

    update

--- homeworks/homework1/plot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.loadtxt('serial.txt')
-size = int(np.sqrt(data.shape[0]))
-data = data.reshape(size, size)
-plt.imshow(data, cmap='hot', interpolation='nearest')
-plt.savefig('serial.png')
-
